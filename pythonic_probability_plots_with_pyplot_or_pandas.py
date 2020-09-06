491f5a50e6eeea6e3e61aa01ce7f8ce972081607
Gilles Pilon <gillespilon13@gmail.com>
2020-09-05 20:38:18 -0500

    Add main()

--- pythonic_probability_plots_with_pyplot_or_pandas.py
+++ pythonic_probability_plots_with_pyplot_or_pandas.py
@@ -22,28 +22,32 @@ x_axis_label = 'Theoretical quantiles (osm)'
 y_axis_label = 'Ordered values (osr)'
 axes_title = 'Normal Probability Plot'
 
-# Generate the data
-y = np.random.normal(0, 1, 100)
-x = list(range(100))
 
-# matplotlib.pyplot
-fig = plt.figure(figsize=figure_width_height)
-ax = fig.add_subplot(111)
-(osm, osr), (slope, intercept, r) = stats.probplot(y, plot=ax, fit=True)
-ax.set_title(axes_title)
-ax.set_xlabel(x_axis_label)
-ax.set_ylabel(y_axis_label)
+def main():
+    # Generate the data
+    y = np.random.normal(0, 1, 100)
+    x = list(range(100))
+    # matplotlib.pyplot
+    fig = plt.figure(figsize=figure_width_height)
+    ax = fig.add_subplot(111)
+    (osm, osr), (slope, intercept, r) = stats.probplot(y, plot=ax, fit=True)
+    ax.set_title(axes_title)
+    ax.set_xlabel(x_axis_label)
+    ax.set_ylabel(y_axis_label)
+    # pandas.plot.scatter
+    df = pd.DataFrame.from_dict({x_axis_label: osm,
+                                 y_axis_label: osr})
+    fig = plt.figure(figsize=figure_width_height)
+    ax = fig.add_subplot(111)
+    ax = df.plot.scatter(
+        x=x_axis_label,
+        y=y_axis_label,
+        title=axes_title,
+        ax=ax
+    )
+    (osm, osr), (slope, intercept, r) = stats.probplot(y, plot=ax, fit=True)
+    ax.plot([-3, 3], [-3, 3], 'r-')
 
-# pandas.plot.scatter
-df = pd.DataFrame.from_dict({x_axis_label: osm,
-                             y_axis_label: osr})
-fig = plt.figure(figsize=figure_width_height)
-ax = fig.add_subplot(111)
-ax = df.plot.scatter(
-    x=x_axis_label,
-    y=y_axis_label,
-    title=axes_title,
-    ax=ax
-)
-(osm, osr), (slope, intercept, r) = stats.probplot(y, plot=ax, fit=True)
-ax.plot([-3, 3], [-3, 3], 'r-')
+
+if __name__ == '__main__':
+    main()
